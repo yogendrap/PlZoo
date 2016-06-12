75b17bdaba467f6a937a926c3f2d88ac64b35f5a
Ajay Singh <meajaysingh@hotmail.com>
2016-06-12 22:40:31 +0530

    chore(storage): redis, mongo connections

--- core/lib/Database/Connection.ts
+++ core/lib/Database/Connection.ts
@@ -1,34 +1,63 @@
 
 import * as Knex from 'knex';
 import * as Bookshelf from 'bookshelf';
+import {createClient as createRedisClient } from 'redis';
+import * as Mongoose from 'mongoose';
+
 import {IConnection} from './IConnection';
 
+
 export default class Connection {
-    private _connection: Bookshelf;
+    private _connection: any;
 
     constructor(dataBaseInfo: IConnection) {
+        let conn = undefined;
         // PostgreSQL, MySQL, and SQLite3.
-        
-        
-        // XXX::: Dont know about port
-        let knex = Knex({
-            client: dataBaseInfo.driver,
-            connection: {
-                host: dataBaseInfo.host,
-                user: dataBaseInfo.username,
-                password: dataBaseInfo.password,
-                database: dataBaseInfo.database,
-                charset: 'utf8'
-            }
-        });
+        switch (dataBaseInfo.driver) {
+            case 'mysql':
+            case 'sqllite3':
+            case 'postgresql':
+                conn = Knex({
+                    client: dataBaseInfo.driver,
+                    connection: {
+                        host: dataBaseInfo.host,
+                        user: dataBaseInfo.username,
+                        password: dataBaseInfo.password,
+                        database: dataBaseInfo.database,
+                        charset: 'utf8'
+                    }
+                });
+                conn = Bookshelf(conn);
+                break;
+            case 'redis':
+                conn = createRedisClient({
+                    host: dataBaseInfo.host,
+                    port: dataBaseInfo.port
+                });
+                break;
+            case 'mongo':
+                conn = Mongoose.connect(
+                    `mongodb://${dataBaseInfo.host}:${dataBaseInfo.port}/${dataBaseInfo.database}`
+                    , function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                break;
+            default:
+                throw new Error(`(${dataBaseInfo.driver}) Driver is not supperted. Please check the settings for storage.`);
+        }
 
-        knex['_config'] = dataBaseInfo;
+        // XXX::: Dont know about port
+
+
+        conn['_config'] = dataBaseInfo;
         // XXX::: todo for mongo
         // XXX::: todo for elastic
-        
-        this._connection = Bookshelf(knex);
+        // XXX::: todo for redis
+        this._connection = conn;
     }
-    getConnection(): Bookshelf {
+    getConnection(): any {
         return this._connection;
     }
     closeConnection(): void {
